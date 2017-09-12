12d1b29409a9e07dc7b521ba84d056c431b8d029
yamanakatsuyoshi <yamanaka.tsuyoshi@piala.co.jp>
2017-09-12 15:18:47 +0900

    maindata

---
+++ public/assets/js/bootstrap-datepicker.ja.js
@@ -0,0 +1,17 @@
+/**
+ * Japanese translation for bootstrap-datepicker
+ * Norio Suzuki <https://github.com/suzuki/>
+ */
+;(function($){
+	$.fn.datepicker.dates['ja'] = {
+		days: ["“ú—j", "Œ—j", "‰Î—j", "…—j", "–Ø—j", "‹à—j", "“y—j"],
+		daysShort: ["“ú", "Œ", "‰Î", "…", "–Ø", "‹à", "“y"],
+		daysMin: ["“ú", "Œ", "‰Î", "…", "–Ø", "‹à", "“y"],
+		months: ["1Œ", "2Œ", "3Œ", "4Œ", "5Œ", "6Œ", "7Œ", "8Œ", "9Œ", "10Œ", "11Œ", "12Œ"],
+		monthsShort: ["1Œ", "2Œ", "3Œ", "4Œ", "5Œ", "6Œ", "7Œ", "8Œ", "9Œ", "10Œ", "11Œ", "12Œ"],
+		today: "¡“ú",
+		format: "yyyy/mm/dd",
+		titleFormat: "yyyy”NmmŒ",
+		clear: "ƒNƒŠƒA"
+	};
+}(jQuery));
